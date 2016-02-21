8159d4351c10c97f58b5e920c9e9f3ea0326397a
Kevin Locke <kevin@kevinlocke.name>
2016-02-21 15:02:19 -0800

    Add an example function to the module
    This example function has my current convention for supporting both
    callbacks and promises.
    
    Signed-off-by: Kevin Locke <kevin@kevinlocke.name>

--- index.js
+++ index.js
@@ -6,4 +6,33 @@
 
 var modulename = {};
 
+modulename.func = function func(options, callback) {
+  if (!callback && typeof options === 'function') {
+    callback = options;
+    options = null;
+  }
+
+  if (!callback && typeof Promise === 'function') {
+    return new Promise(function(resolve, reject) {
+      func(options, function(err, result) {
+        if (err) reject(err); else resolve(result);
+      });
+    });
+  }
+
+  if (typeof callback !== 'function') {
+    throw new TypeError('callback must be a function');
+  }
+
+  if (options && typeof options !== 'object') {
+    process.nextTick(function() {
+      callback(new TypeError('options must be an object'));
+    });
+    return undefined;
+  }
+
+  // Do stuff
+  return undefined;
+};
+
 module.exports = modulename;
