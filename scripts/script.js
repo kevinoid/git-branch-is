3c7a0bcadd919a2ece5a0719ff704b580ed27910
Kevin Locke <kevin@kevinlocke.name>
2019-03-26 11:32:13 -0600

    scripts/script.js: Use bin/cmd.js conventions
    Apply the same simplifications to scripts/script.js as bin/cmd.js to
    allow passing process and process.exit directly and remove the automatic
    Promise-ification.
    
    Signed-off-by: Kevin Locke <kevin@kevinlocke.name>

--- scripts/script.js
+++ scripts/script.js
@@ -8,55 +8,21 @@
 
 'use strict';
 
-/** Entry point for this command.
+/** Entry point for this script.
  *
- * @param {!Array<string>} args Command-line arguments.
- * @param {CommandOptions=} options Options.
- * @param {?function(Error, number=)=}
- * callback Callback for the exit code or an <code>Error</code>.
- * @return {Promise<number>|undefined} If <code>callback</code> is not given,
- * a <code>Promise</code> with the exit code or <code>Error</code>.
+ * @param {Array<string>} args Command-line arguments.
+ * @param {!CommandOptions} options Options.
+ * @param {function(number)} callback Callback with exit code.
  */
 function scriptname(args, options, callback) {
-  if (!callback && typeof options === 'function') {
-    callback = options;
-    options = null;
-  }
-
-  if (!callback) {
-    return new Promise((resolve, reject) => {
-      scriptname(args, (err, result) => {
-        if (err) { reject(err); } else { resolve(result); }
-      });
-    });
-  }
-
-  if (typeof callback !== 'function') {
-    throw new TypeError('callback must be a function');
-  }
-
-  return undefined;
 }
 
 module.exports = scriptname;
 
 if (require.main === module) {
   // This file was invoked directly.
-  /* eslint-disable no-process-exit */
-  const mainOptions = {
-    in: process.stdin,
-    out: process.stdout,
-    err: process.stderr,
-  };
-  scriptname(process.argv, mainOptions, (err, code) => {
-    if (err) {
-      if (err.stdout) { process.stdout.write(err.stdout); }
-      if (err.stderr) { process.stderr.write(err.stderr); }
-      process.stderr.write(`${err.name}: ${err.message}\n`);
-
-      code = typeof err.code === 'number' ? err.code : 1;
-    }
-
-    process.exit(code);
+  // Note:  Could pass process.exit as callback to force immediate exit.
+  scriptname(process.argv, process, (exitCode) => {
+    process.exitCode = exitCode;
   });
 }
