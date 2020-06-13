6247bed9ed00411f78cde855b3f2ad5687c374ee
Kevin Locke <kevin@kevinlocke.name>
2020-06-13 17:47:20 -0600

    Move global hooks to root hooks
    So that they will work with Mocha's parallel execution mode.
    
    Signed-off-by: Kevin Locke <kevin@kevinlocke.name>

---
+++ test-lib/root-hooks.js
@@ -0,0 +1,22 @@
+/**
+ * @copyright Copyright 2016-2020 Kevin Locke <kevin@kevinlocke.name>
+ * @license MIT
+ */
+
+'use strict';
+
+const { TEST_REPO_PATH } = require('./constants');
+
+// Working directory before changing to TEST_REPO_PATH
+let origCWD;
+
+exports.mochaHooks = {
+  beforeAll() {
+    origCWD = process.cwd();
+    process.chdir(TEST_REPO_PATH);
+  },
+
+  afterAll() {
+    process.chdir(origCWD);
+  },
+};
